d8c423d8d8e4d9d0c8f9575d95823ff6e5ed726f
teiwiet <teiwiet33@gmail.com>
2026-05-06 01:33:32 +0700

    feat : add necessary struct & enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+struct  Server{
+    address : String,
 }
+
+impl Server{
+    fn new(ip_address:String) -> Server {
+        Server{
+            address : ip_address,
+        }
+    }
+    fn run(self){
+        println!("Listening on {}",self.address);
+    }
+}
+
+struct Request{
+    path: String,
+    query_string: String,
+    method: String
+}
+
+enum Method{
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH
+}
+
+fn main() {
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
+
+
